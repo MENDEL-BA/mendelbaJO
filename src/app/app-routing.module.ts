37cca0a89635a75701b8e42f4add6a35593ff51b
Mendel <mendel@MacBook-Pro-de-Mendel.local>
2023-09-04 13:39:17 +0200

    Ajout de commentaire sur les routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetailComponent } from './detail/detail.component';
 
+// Mise en place des routes (url)
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomeComponent, // rediriger automatiquement sur la page acceuil
   },
   {
     path: 'details/:name',
@@ -15,7 +16,7 @@ const routes: Routes = [
   },
   {
     path: '**', // wildcard
-    component: NotFoundComponent,
+    component: NotFoundComponent,// si url n'existe pas rediriger sur la page notfound
   },
 ];
 
